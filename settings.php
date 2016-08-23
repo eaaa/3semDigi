132737e8a991ecfdae3baa8db75d849ffa55c45e
Bror-Arnfast <2012.github@arnfast.dk>
2016-08-23 07:38:18 +0200

    Initial commit of BrorNameGame

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php 
+	$title = "My favourite class 3.sem. Digi";
+	$sti = $_SERVER['DOCUMENT_ROOT'].'/yourfoldernamehere/Pics';
+	$sti = "./Pics/small"; // delete this line if your PHP requires more specific folder id (above)
+	
+?>
