179ba4556ca692d4935ae616dce36fe68e593736
Bror Strange Arnfast (lektor – bror@eaaa.dk) <bror@eaaa.dk>
2016-08-24 12:53:36 +0200

    Comment changed for clarification

--- settings.php
+++ settings.php
@@ -2,6 +2,6 @@
 	$title = "My favourite class 3.sem. Digi";
 	$sti = $_SERVER['DOCUMENT_ROOT'].'/yourfoldernamehere/Pics/small';
 	$sti = "./Pics/small"; // delete this line if your PHP 
-					//  requires more specific folder id (as seen above)
+					//  requires more specific folder id (as in line 3)
 	
 ?>
