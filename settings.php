7ecd23c2690bf19a5baf6ebd3110f13f6085c9b9
Jacob Thesbjerg <jacobthesbjerg@gmail.com>
2016-08-29 15:49:51 +0200

    Rewrote function for displaying images. It now checks for file extensions. Added css/style.css Moved styling to css/style.css Changed styling for cover box Changed variable name for $sti to $dir Deleted comments in buttons.php

--- settings.php
+++ settings.php
@@ -1,7 +1,9 @@
-<?php 
-	$title = "My favourite class 3.sem. Digi";
-	$sti = $_SERVER['DOCUMENT_ROOT'].'/yourfoldernamehere/Pics/small';
-	$sti = "./Pics/small"; // delete this line if your PHP 
-					//  requires more specific folder id (as in line 3)
-	
+<?php
+// Changed title
+$title = "NameGame";
+// Changed variable name to keep variable names in English
+$dir = $_SERVER['DOCUMENT_ROOT'].'/3semdigi/Pics/small';
+$dir = "./Pics/small"; // delete this line if your PHP
+				//  requires more specific folder id (as in line 3)
+
 ?>
